4ca179cdfc8e6dcc25f54fe2d3911e9c99642c1e
hustwl159 <hustwl159@qq.com>
2024-02-24 21:10:16 +0800

    2024年2月24日

--- douyin/douyin/spiders/collection.py
+++ douyin/douyin/spiders/collection.py
@@ -44,7 +44,7 @@ class CollectionSpider(scrapy.Spider):
         for beauty in beautylist:
             items = DouyinItem()
             num = num + 1
-            if num == 118:
+            if num == 53:
                 break
             if beauty.xpath('./div/a/@href').get()[1:6] == 'video':
                 items['beautylink'] = 'https://www.douyin.com' + beauty.xpath('./div/a/@href').get()
